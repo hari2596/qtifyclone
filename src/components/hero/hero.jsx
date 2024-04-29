a58e49fe1b572e52e5863ee71df19c503f02fe10
hari2596 <hariprasadkg96@gmail.com>
2024-04-29 21:52:55 +0530

    new

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                 <h1 className={styles.head}>100 Thousand Songs, ad-free</h1>
                 <h1 className={styles.head}>Over thousands podcast episodes</h1>
                 <div  className={styles.line}> <hr/></div>
-                <h1 className={styles.myHead}>AB's QTify</h1>
+              
                 <div  className={styles.line2}> <hr/></div>
             </div>
                 <img className={styles.headphone} src={HeroImage} alt='headphone'/>
